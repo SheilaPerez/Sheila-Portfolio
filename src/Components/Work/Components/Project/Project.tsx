c53f4d62351826166bcebab40519de314b6007f2
SheilaPerez <sheilapg26@gmail.com>
2022-11-22 21:22:20 +0100

    movileFixed

--- src/Components/Work/Components/Project/Project.tsx
+++ src/Components/Work/Components/Project/Project.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { FaGithub } from "react-icons/fa";
 import './Project.css';
 interface Props {
     handleClickOpenProject: (projectName: string) => void;
@@ -11,14 +12,21 @@ interface Props {
 const Project: FC<Props> = ({handleClickOpenProject, projectName, descriptionProject, techsDescription, projectStyle}) => {
 
     return (
-        <div className="projectContent" onClick={() => handleClickOpenProject(projectName)}>
-            <div className={`${projectStyle} ${"imageStyle"}`}></div>
+        <div className="projectContent" >
+            <div onClick={() => handleClickOpenProject(projectName)}>
+                <div className={`${projectStyle} ${"imageStyle"}`}></div>
+            </div>
             <div className="explicationContent">
                 <p className="titleProject">{projectName}</p>
                 <p className="descriptionProject">{descriptionProject}</p>
-                <p className="descriptionProject">{techsDescription}</p>
+                <div className='flexGithub'>
+                    <p className="descriptionProject">{techsDescription} </p>
+                    <p className='gitIcon'><FaGithub size={20} /></p>
+                </div>
             </div>
         </div>
+        
+
     )
 };
 
